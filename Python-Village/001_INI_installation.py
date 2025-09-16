e0737821226e06933d6b38c7c824ac372491546a
Ted Ngaleu <tedngaleu@tedngaleu.com>
2025-09-16 02:58:46 +0000

    Add files via upload

---
+++ Python-Village/001_INI_installation.py
@@ -0,0 +1,11 @@
+# Rosalind Reference Solutions — Title Page
+# Problem: INI1 — Installing Python
+# Repository: Rosalind-Reference-Solutions
+
+# Author: Ted Ngaleu
+# Affiliation: NIA Postbac Fellow, Vascular Aging Biology Unit
+# Created: 2025-09-15
+# Last Updated: 2025-09-15
+# License: MIT © 2025 Ted Ngaleu
+
+import this
